1036cb77ae966084d4ee1069a15d0f449bfb2d21
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-23 23:04:57 +1000

    Cover the main logic with unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,15 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = describe "Math" $ do
-    it "addition" $
-        1 + 1 `shouldBe` 2
+spec = describe "commit-msg" $ do
+    it "adds author's name" $
+        extendCommitMessage originalMessage `shouldBe` "[ryuichi]\n" ++ originalMessage
+
+originalMessage = "# Please enter the commit message for your changes. Lines starting\n\
+                  \# with '#' will be ignored, and an empty message aborts the commit.\n\
+                  \#\n\
+                  \# On branch master\n\
+                  \# Changes to be committed:\n\
+                  \#\tnew file:   sample.txt\n\
+                  \#\n\
+                  \"
