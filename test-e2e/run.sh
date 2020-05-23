fb2560c7d8f3a0c1c2cf604e04cd0be70fd830a2
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-23 20:26:07 +1000

    Created end-to-end test

---
+++ test-e2e/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+this_dir="$(dirname "$BASH_SOURCE")"
+project_root="$this_dir/.."
+test_tmp_dir="$this_dir/tmp"
+
+tmp_file="$test_tmp_dir/commit-msg.txt"
+
+rm -rf "$test_tmp_dir" && mkdir -p "$test_tmp_dir"
+cp "$project_root/fixtures/sample.txt" "$tmp_file"
+
+(cd "$project_root" && stack exec -- commit-msg-exe "$tmp_file" && diff "$tmp_file" "$this_dir/expected.txt")
