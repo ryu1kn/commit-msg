924d009b558d84c088c9d99195461eeaff05bfdf
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-24 23:22:58 +1000

    Refactoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,15 +7,15 @@ import Data.List
 
 extendCommitMessage :: Config -> String -> String
 extendCommitMessage conf msg =
-  let header = (++) <$> authorSection <*> taskIds $ conf
+  let header = prepopulated conf
       separator = if length header > 0 then "\n" else ""
    in header ++ separator ++ msg
 
-authorSection :: Config -> String
-authorSection = maybe "" groupedValue . authors
+prepopulated :: Config -> String
+prepopulated = (++) <$> (groupValue . authors) <*> (groupValue . task_ids)
 
-taskIds :: Config -> String
-taskIds = maybe "" groupedValue . task_ids
-
-groupedValue :: [String] -> String
-groupedValue xs = if length xs > 0 then "[" ++ (intercalate "," xs) ++ "]" else ""
+groupValue :: Maybe [String] -> String
+groupValue = maybe "" groupedValue_
+    where
+        groupedValue_ :: [String] -> String
+        groupedValue_ xs = if length xs > 0 then "[" ++ (intercalate "," xs) ++ "]" else ""
