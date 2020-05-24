ef1c86f850762264835d259d1763f30081c329b4
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-24 16:24:58 +1000

    Use either to avoid nesting structure

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ main = do
     message_file <- head <$> getArgs
     contents <- C.readFile message_file
     config <- decodeFileEither ".commit-msg.yaml" :: IO (Either ParseException Config)
-    case config of
-        Right conf -> C.writeFile message_file . C.pack . extendCommitMessage conf . C.unpack $ contents
-        Left err -> putStrLn $ prettyPrintParseException err
+    either (putStrLn . prettyPrintParseException) (updateFile message_file contents) config
+
+updateFile :: String -> C.ByteString -> Config -> IO ()
+updateFile message_file contents conf = C.writeFile message_file . C.pack . extendCommitMessage conf . C.unpack $ contents
