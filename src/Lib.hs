c9264a01d72af3dce1ce292aed5b884b3abc30e9
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-24 22:31:21 +1000

    Deal with task IDs

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,4 +6,13 @@ import Config
 import Data.List
 
 extendCommitMessage :: Config -> String -> String
-extendCommitMessage conf msg = "[" ++ (intercalate "," $ authors conf) ++ "]\n" ++ msg
+extendCommitMessage conf msg = authorSection conf ++ taskIds conf ++ "\n" ++ msg
+
+authorSection :: Config -> String
+authorSection = groupedValue . authors
+
+taskIds :: Config -> String
+taskIds = maybe "" groupedValue . task_ids
+
+groupedValue :: [String] -> String
+groupedValue xs = if length xs > 0 then "[" ++ (intercalate "," xs) ++ "]" else ""
