feb95d6388a4b3c8030a30d7f618c31df8b0f78f
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-24 17:31:37 +1000

    Make it concise

--- app/Main.hs
+++ app/Main.hs
@@ -11,10 +11,10 @@ main = do
     message_file <- head <$> getArgs
     contents <- byteReadFile message_file
     config <- decodeFileEither ".commit-msg.yaml" :: IO (Either ParseException Config)
-    either (putStrLn . prettyPrintParseException) (updateFile message_file contents) config
+    either (putStrLn . prettyPrintParseException) (byteWriteFile message_file . editMessage contents) config
 
-updateFile :: FilePath -> String -> Config -> IO ()
-updateFile message_file contents conf = byteWriteFile message_file . extendCommitMessage conf $ contents
+editMessage :: String -> Config -> String
+editMessage = flip extendCommitMessage
 
 byteReadFile :: FilePath -> IO String
 byteReadFile = fmap C.unpack . C.readFile
