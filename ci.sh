de9e34d0a9c22db5eff837bba5969c512229ee40
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-25 23:44:06 +1000

    Renamed the ci build script

--- ci.sh
+++ ci.sh
@@ -2,10 +2,7 @@
 
 set -euo pipefail
 
-# DEBUG: investigating stack error
-id
-ls -ld "$(pwd)"
-ls -ld "/github/home"
+# HACK: Prevent stack aborting with "You are not the owner of ..." error
 mkdir -p /github/home/.stack/
 
 stack test --allow-different-user
